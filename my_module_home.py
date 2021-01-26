4fa27ae642a1fe58f0e5feb2978239e08056379b
igor91m <igor91m@yandex.ru>
2021-01-26 17:49:39 +0300

    add changes

--- my_module_home.py
+++ my_module_home.py
@@ -6,8 +6,8 @@ def salary():
         tax_13 = 0.13
         sum_nalog = ((output_in_hours * rate) + bonus) * tax_13
         salary_without_nalog = (output_in_hours * rate) + bonus
-        total_sum = salary_without_nalog - sum_nalog
+        total_salary = salary_without_nalog - sum_nalog
         print(f"Заработная плата сотрудника до вычета налога: {round(salary_without_nalog, 2)} рублей")
-        print(f"Заработная плата сотрудника после вычета налога: {round(total_sum, 2)} рублей")
+        print(f"Заработная плата сотрудника после вычета налога: {round(total_salary, 2)} рублей")
     except ZeroDivisionError:
         return print("Не те числа")
