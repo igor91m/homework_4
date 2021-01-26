0f4accf11ee6ddea3e73dd0f34131e61f5a2311d
igor91m <igor91m@yandex.ru>
2021-01-26 18:19:55 +0300

    add changes

--- my_module_home.py
+++ my_module_home.py
@@ -4,10 +4,11 @@ def salary():
         rate = float(input("Ставка в час: "))
         bonus = float(input("Премия: "))
         tax_13 = 0.13
-        sum_nalog = ((output_in_hours * rate) + bonus) * tax_13
-        salary_without_nalog = (output_in_hours * rate) + bonus
-        total_salary = salary_without_nalog - sum_nalog
-        print(f"Заработная плата сотрудника до вычета налога: {round(salary_without_nalog, 2)} рублей")
+        sum_tax = ((output_in_hours * rate) + bonus) * tax_13
+        salary_without_tax = (output_in_hours * rate) + bonus
+        total_salary = salary_without_tax - sum_tax
+        print(f"Заработная плата сотрудника до вычета налога: {round(salary_without_tax, 2)} рублей")
+        print(f"Налог составил: {round(sum_tax, 2)} рублей")
         print(f"Заработная плата сотрудника после вычета налога: {round(total_salary, 2)} рублей")
     except ZeroDivisionError:
         return print("Не те числа")
