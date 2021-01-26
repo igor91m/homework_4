d126a3bb3260d1bab7b0e633c919705cd8135340
igor91m <igor91m@yandex.ru>
2021-01-26 19:27:54 +0300

    add changes

--- task_6.py
+++ task_6.py
@@ -9,10 +9,20 @@
 Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
 Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
 '''
-
+'''
 # а) Итератор, генерирующий целые числа, начиная с указанного
 from itertools import count
 for el in count(int(input("Введите целое число отсчёта: "))):
     if el > 17:
         break
     print(el)
+'''
+# б) Итератор, повторяющий элементы некоторого списка, определенного заранее
+from itertools import cycle
+
+list = [777, 'AvadaKedavra', True, None]
+iterator = cycle(list)
+print(next(iterator))  # 777
+print(next(iterator))  # AvadaKedavra
+print(next(iterator))  # True
+print(next(iterator))  # None
