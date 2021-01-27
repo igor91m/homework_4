2c79c50a4e969af59d184db3b7c4deef30d75082
igor91m <igor91m@yandex.ru>
2021-01-27 03:16:25 +0300

    add changes

--- task_7.py
+++ task_7.py
@@ -6,4 +6,33 @@
 
 Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 '''
+from itertools import count
+from math import factorial
+'''
+Не могу понять как прописать с условием for el in fact(n) по самой задаче с счетчиком
+'''
 
+import math
+print(math.factorial(5))
+
+'''
+Другие примеры:
+
+#1
+n = int(input("Введите факториал какого числа найти: "))
+fact = 1
+while n > 1:
+    fact *= n
+    n -= 1
+print(fact)
+
+#2
+from math import factorial
+print(f"Факториал числа: {factorial(int(input()))}")
+
+#3
+import math
+n = int(input("Введите факториал какого числа найти: "))
+res = math.factorial(n)
+print(res)
+'''
